6867f60f3dd51a21703feeafb787dd6fb76a465e
Wulfy <wulf38@gmail.com>
2018-05-28 16:48:30 -0400

    Use virtual scene numbers for overrides in non-generic grottos

--- ASM/src/config.asm
+++ ASM/src/config.asm
@@ -23,6 +23,14 @@
 ;   0x84: Lost Woods: under boulder next to Goron City warp
 ;   0x87: Death Mountain: SoS hole outside Goron City
 ;   0x8A: Death Mountain Crater: under boulder in circle of rocks near top entrance
+;
+; Other grotto virtual scene numbers:
+;   0x71: Hyrule Field: Underwater heart piece
+;   0x76: Hyrule Field: Salesman grotto
+;   0x77: Redead grotto
+;   0x7D: Wolfos grotto
+;   0x83: Deku Theater
+;   0x85: Lost Woods: Salesman grotto
 
 .area 0x200, 0
 ITEM_OVERRIDES:
