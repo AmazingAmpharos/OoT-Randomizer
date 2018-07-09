091b5b6188f6d50067574ad81197f26e0cd20f3f
Aharon Turpie <aaturpie@verizon.net>
2018-07-09 01:46:45 -0400

    Fix bombchu crash

--- Rom.py
+++ Rom.py
@@ -1125,9 +1125,9 @@ def patch_rom(world, rom):
 
         #Fix bombchu chest animations
         chestAnimations = {
-            0x6A: 0x18, #0xD8 #Bombchu (5) 
-            0x03: 0x18, #0xD8 #Bombchu (10)    
-            0x6B: 0x18, #0xD8 #Bombchu (20)    
+            0x6A: 0x28, #0xD8 #Bombchu (5) 
+            0x03: 0x28, #0xD8 #Bombchu (10)    
+            0x6B: 0x28, #0xD8 #Bombchu (20)    
         }
         for item_id, gfx_id in chestAnimations.items():
             rom.write_byte(0xBEEE8E + (item_id * 6) + 2, gfx_id)
