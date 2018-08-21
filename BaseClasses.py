3d916bd6edbcf8a4ace6b49a1250217d737216d1
r0bd0g <42326313+r0bd0g@users.noreply.github.com>
2018-08-21 14:13:02 -0300

    Logic Fixes - Biggoron, Mask of Truth, Night Skulls

--- BaseClasses.py
+++ BaseClasses.py
@@ -336,7 +336,7 @@ class CollectionState(object):
         return (self.world.open_forest or (self.has('Slingshot') and self.has('Kokiri Sword')))
 
     def can_finish_adult_trades(self):
-        zora_thawed = self.has_bottle() and self.has('Zeldas Lullaby') and (self.can_reach('Ice Cavern') or self.can_reach('Ganons Castle Water Trial') or self.has('Progressive Wallet', 2))
+        zora_thawed = self.has_bottle() and (self.can_play('Zeldas Lullaby') or (self.has('Hover Boots') and self.world.logic_zora_with_hovers)) and (self.can_reach('Ice Cavern') or self.can_reach('Ganons Castle Water Trial') or self.has('Progressive Wallet', 2))
         carpenter_access = self.has('Epona') or self.has('Progressive Hookshot', 2)
         return (self.has('Claim Check') or ((self.has('Eyedrops') or self.has('Eyeball Frog') or self.has('Prescription') or self.has('Broken Sword')) and zora_thawed) or ((self.has('Poachers Saw') or self.has('Odd Mushroom') or self.has('Cojiro') or self.has('Pocket Cucco') or self.has('Pocket Egg')) and zora_thawed and carpenter_access))
 
@@ -368,7 +368,7 @@ class CollectionState(object):
     def guarantee_hint(self):
         if(self.world.hints == 'mask'):
             # has the mask of truth
-            return self.has('Zeldas Letter') and self.has('Sarias Song') and self.has('Kokiri Emerald') and self.has('Goron Ruby') and self.has('Zora Sapphire')
+            return self.has('Zeldas Letter') and self.can_play('Sarias Song') and self.has('Kokiri Emerald') and self.has('Goron Ruby') and self.has('Zora Sapphire')
         elif(self.world.hints == 'agony'):
             # has the stone of agony
             return self.has('Stone of Agony')
@@ -376,7 +376,7 @@ class CollectionState(object):
 
     def nighttime(self):
         if self.world.logic_no_night_tokens_without_suns_song:
-            return self.has('Suns Song')
+            return self.can_play('Suns Song')
         return True
 
     def collect(self, item):
