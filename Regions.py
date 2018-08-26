5d8a123f90731df6a531d4d6501767276be222c7
Alex Trush <epicYoshi64@gmail.com>
2018-08-26 01:18:52 -0700

    Clock Town Cleanup
    Separated Clock Town back into the N,S,W,E separated regions. still need to add locations to the location table, and tie the regions together in EntranceShuffle.py

--- Regions.py
+++ Regions.py
@@ -5,43 +5,91 @@ from BaseClasses import Region, Location, Entrance, RegionType
 def create_regions(world):
 
     world.regions = [
-        create_interior_region('Pre Game Deku Challenge Room', ['First Nuts'], ['Clock Tower']),
-        create_interior_region('Clock Tower', ['Remove the Cursed Mask', 'Song from Mask Salesman'], ['Clock Tower Exit', 'Prologue Area']),
-        create_ow_region('Clock Town', ['Clock Town SF' 'Heart up a Tree', 'Bombers Tag Reward', 'Sakon Definitely Wasnt Up To Something', 'Deliver Anjus Letter', 'A Postmans Work Is Never Done', 'Spread the Dance of Kamaro'] +
-                            ['Saving Up For A New Wallet', 'Big Money In Da Bank', 'Tingles Town Map', 'Tingles Woodfall Map', 'Tell Town Frog Spring Has Come'],
-                            ['Clock Tower', 'Clock Tower Balcony', 'Clock Town Guards', 'Bombers Tunnel', 'Curiosity Shop Backroom', 'Curiosity Shop', 'Trading Post', 'Bomb Shop', 'Post Office', 'Lottery Shop', 'Swordsmans School'] +
-                            ['GF Clock Town', 'Deku Playground', 'Honey and Darling', 'Treasure Chest Shop', 'Town Shooting Gallery', 'Milk Bar', 'Stock Pot Inn', 'Mayors Office', 'ECT Shop Rooftops']),
-        create_ow_region('Clock Tower Balcony', ['Clock Tower Balcony Prize'], ['Clock Town', 'Clock Tower Rooftop']),
-        create_interior_region('Clock Tower Rooftop', ['Dropped Ocarina', 'Memory of the Song of Time'], []),
-        create_interior_region('Bomber Tunnel', ['Bomber Bomb Chest', 'Moon Cry'], ['Clock Town', 'Astral Observatory Deck']),
-        create_ow_region('Astral Observatory Deck', ['Moons Sad Crater'], ['Bomber Tunnel', 'Astral Observatory Fence']),
-        create_interior_region('Astral Observatory Fence', [], ['Astral Observatory Deck', 'Termina Field']),
-        create_ow_region('Clock Town Guards', [], ['Clock Town', 'Termina Field']),
-        create_ow_region('ECT Shop Rooftops', ['ECT Silver Rupee'], 'Clock Town'),
-        create_interior_region('Honey and Darling', ['Honey and Darling Bombchu Bowling Prize', 'Honey and Darling Archery Prize', 'Honey and Darling Basket Bomb Throw Prize', 'Honey and Darling Grand Champion'], ['Clock Town']),
-        create_interior_region('Town Shooting Gallery', ['Town Shooting Beat the Record', 'Town Shooting Perfect Score'], ['Clock Town']),
-        create_interior_region('Mayors Office', ['Expert Person Solver Takes the Case', 'Love is the True Mayor'], ['Clock Town']),
-        create_interior_region('Stock Pot Inn', ['Do You Have a Reservation?', 'Have you seen this man?', 'What is the Carnival of Time?', 'Who are the Four Giants?', 'Help I fell in', 'Theres a Chest in my Bed', 'Stealing From Anjus Chest', 'The Couple is Reunited at Long Last'], ['Clock Town']),
+        create_interior_region('Clock Tower',
+            ['Remove the Cursed Mask', 'Song from HMS'],
+            ['Clock Tower Exit', 'Clock Tower Twisted Hallway']),
+        create_interior_region('Prologue Room', ['First Nuts']),
+
+        create_ow_region('South Clock Town',
+            ['SCT 20 Rupee Chest', 'SCT 50 Rupee Chest',
+            'Town Deku Salesman', 'Clock Town Owl Statue', 'Love on the Balcony'],
+            ['Clock Tower Entrance', 'Clock Tower Balcony Entrance', 'South Mailbox',
+            'SCT Top Exit to WCT', 'SCT Bottom Exit to WCT', 'SCT Exit to NCT',
+            'SCT Exit to NCT', 'SCT Bottom Exit to ECT', 'SCT Top Exit to ECT',
+            'Clock Town South Gate', 'SCT Exit to Laundry Pool']),
+        create_interior_region('Clock Tower Rooftop', ['Dropped Ocarina', 'Song from Skull Kid']),
+
+        create_ow_region('East Clock Town',
+            ['Clock Town SF', 'Free The Postman',
+            'ECT 100 Rupee Chest', 'Delivery to Mama Kafei'],
+            ['Honey and Darling', 'Treasure Chest Shop', 'Town Shooting Gallery', 'East Mailbox',
+            'Milk Bar', 'Stock Pot Inn', 'Stock Pot Inn Secret Entrance', 'Mayors Office', 'Bombers Tunnel']),
+        create_interior_region('Honey and Darling',
+            ['Honey and Darling Bombchu Bowling Prize', 'Honey and Darling Archery Prize',
+            'Honey and Darling Basket Bomb Throw Prize', 'Honey and Darling Grand Champion'],
+            ['Honey and Darling Exit']),
         create_interior_region('Treasure Chest Shop', ['Treasure Chest Game Piece of Heart Prize'], ['Clock Town']),
+        create_interior_region('Town Shooting Gallery', ['Town Shooting Beat the Record', 'Town Shooting Perfect Score'], ['Town Shooting Gallery Exit']),
+        create_interior_region('Milk Bar', ['Bar Performance'], ['Milk Bar Exit']),
+        create_interior_region('Stock Pot Inn',
+            ['Do You Have a Reservation?', 'Have you seen this man?',
+            'What is the Carnival of Time?', 'Who are the Four Giants?',
+            'Love in the Toilet?', 'Stock Pot Reservation Chest', 'Anjus Chest',
+            'We Shall Greet The Morning Together'],
+            ['Stock Pot Inn Roof Exit', 'Stock Pot Inn Front Door']),
+        create_interior_region('Mayors Office', ['Expert Person Solver Takes the Case', 'Love is the True Mayor'], ['Mayors Office Exit']),
+        create_interior_region('Bomber Tunnel', ['Bomber Tunnel Chest'], ['Bomber Tunnel Exit', 'Astral Observatory'])
+        create_interior_region('Astral Observatory', ['Moon Cry'], ['Bomber Tunnel', 'Astral Observatory Exit']),
+        create_ow_region('Astral Observatory Deck', ['Moons Sad Crater'], ['Bomber Tunnel', 'Astral Observatory Fence']),
+
+        create_ow_region('West Clock Town',
+            ['200 Rupee Prize', '5000 Rupee Prize', 'Spread the Dance of Kamaro',
+            'Hidden Owl Statue'],
+            ['Curiosity Shop', 'Trading Post', 'Bomb Shop',
+            'Post Office', 'Lottery Shop', 'Swordsmans School',
+             'Clock Town West Gate', 'WCT Top Exit to SCT', 'WCT Bottom Exit to SCT']),
+        create_ow_region('Mailbox', ['Deliver Letter to Kafei'])
+        create_interior_region('Curiosity Shop', ['Buying The Overpriced Mask'], ['Curiosity Shop Exit']),
+        create_interior_region('Trading Post', [], ['Trading Post Exit']),
+        create_interior_region('Bomb Shop', ['Buy Bomb Bag', 'Buy Bigger Bomb Bag'], ['Bomb Shop Exit']),
+        create_interior_region('Post Office', ['The Highest Priority of Mails', 'Counting Is Hard'], ['Post Office Exit']),
+        create_interior_region('Lottery Shop', [], ['Lottery Shop Exit']),
+        create_interior_region('Swordsmans School', ['Expert Jump Slash Execution'], ['Swordsmans School Exit']),
+
+        create_ow_region('North Clock Town',
+            [ 'Bombers Tag Reward', 'Love on a Tree', 'Tingle Town Map',
+            'Tingle Woodfall Map', 'Foil Sakon'],
+            ['GF Clock Town', 'Deku Playground', 'Clock Town North Gate',
+            'NCT Exit to SCT', 'NCT Exit to ECT', 'North Mailbox']),
         create_interior_region('GF Clock Town', ['Clock Town GF Reward'], ['Clock Town']),
         create_interior_region('Deku Playground', ['Deku Challenge Day 1', 'Deku Challenge Day 2', 'Deku Challenge Day 3', 'Master of the Deku Playground'], ['Clock Town']),
+
+        create_ow_region('Laundry Pool',
+            ['Speak To Town Frog', 'Listen To A Grownup Complain'],
+            ['Curiosity Backroom Entrance', 'Laundry Pool Exit to SCT']),
         create_interior_region('Curiosity Shop Backroom', ['Deliver This To Anju', 'Kafei Left A Mask', 'Kafei Left A Letter'], ['Clock Town']),
-        create_interior_region('Curiosity Shop', ['Buying The Overpriced Mask'], ['Clock Town']),
-        create_interior_region('Trading Post', [], ['Clock Town']),
-        create_interior_region('Bomb Shop', ['Buy Bomb Bag', 'Buy Bigger Bomb Bag'], ['Clock Town']),
-        create_interior_region('Post Office', ['The Highest Priority of Mails', 'Counting Is Hard'], ['Clock Town']),
-        create_interior_region('Lottery Shop', [], ['Clock Town']),
-        create_interior_region('Swordsmans School', ['Expert Jump Slash Execution'], ['Clock Town']),
-        create_ow_region('Termina Field', ['Learn Kamaros Dance', 'TF Chest In The Grass', 'TF Chest On A Stump'], ['Path To Mountain Village', 'Path To Southern Swamp', 'Path To Great Bay', 'Path To Great Bay', 'Path To Ikana', 'Sleeping Peahat Grotto', 'Bees In A Pond Grotto', 'Swamp Gossips', 'Mountain Gossips', 'Ocean Gossips', 'Canyon Gossips', 'Dodongo Grotto']),
+
+        create_ow_region('Termina Field',
+            ['Learn Kamaros Dance', 'TF Chest In The Grass', 'TF Chest On A Stump'],
+            ['Path To Mountain Village', 'Path To Southern Swamp',
+            'Path To Great Bay', 'Path To Great Bay', 'Path To Ikana',
+            'Sleeping Peahat Grotto', 'Bees In A Pond Grotto',
+            'Swamp Gossips', 'Mountain Gossips', 'Ocean Gossips',
+            'Canyon Gossips', 'Dodongo Grotto']),
+
         create_ow_region('Path to Swamp', ['Bat Guarded Tree Treasure'], ['Termina Field', 'Swamp Shooting Gallery', 'Southern Swamp']),
         create_ow_region('Southern Swamp', ['Swamp Tourist Roof Love'], ['Lost Woods', 'Potion Shop', 'Swamp Big Octo', 'Tourist Centre Big Octo', 'Swamp Tourist Cenre']),
         create_interior_region('Swamp Tourist Centre', ['Swamp Tourist Free Product', 'Pictograph Contest Winner'], ['Boat Ride', 'Southern Swamp']),
-        create_ow_region('Boat Ride', [], ['Poison Swamp']),
+        create_ow_region('Boat Ride', None, ['Poison Swamp']),
         create_ow_region('Woodfall Owl Platform', [], ['Woodfall Temple Entrance', 'GF Woodfall', 'Woodfall']),
         create_interior_region('GF Woodfall', ['Woodfall GF Reward'], ['Woodfall']),
+
         create_dungeon_region('Woodfall Temple Entrance', [], []),
+
         create_interior_region('GF Snowhead', ['Snowhead GF Reward'], ['Snowhead Spire']),
+
         create_interior_region('GF Great Bay', ['Great Bay GF Reward'], ['Great Bay Fairy Ledge']),
+
         create_interior_region('GF Stone Tower', ['Stone Tower GF Reward'], ['Ikana Canyon'])
     ]
     world.intialize_regions()
@@ -73,7 +121,9 @@ def _create_region(name, type, locations=None, exits=None):
     return ret
 
 location_table = {'Deku Mask': (None, None, None, 'NPC'),
-                    '20 Rupee SCT': (None, None, None, 'Chest'),
+                    'SCT 20 Rupee Chest': (None, None, None, 'Chest'),
+                    'SCT 50 Rupee Chest': (None, None, None, 'Chest'),
+                    'ECT 100 Rupee Chest': (None, None, None, 'Chest'),
                     'WF SF1': (None, None, None, 'SFF-WF'),
                     'WF SF2': (None, None, None, 'SFC-WF'),
                     'WF SF3': (None, None, None, 'SF-WF'),
