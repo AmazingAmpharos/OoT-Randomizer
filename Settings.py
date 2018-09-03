a072e7cabddd1b8f11cf9b52ec0f1c0159adfab0
scatterclegge <christianlegge888@gmail.com>
2018-09-03 12:47:00 -0230

    made custom colors work with cli

--- Settings.py
+++ Settings.py
@@ -155,6 +155,25 @@ class Settings():
         self.sanatize_seed()
         self.numeric_seed = self.get_numeric_seed()
 
+def parse_custom_tunic_color(s):
+    if s == 'Custom Color':
+        raise argparse.ArgumentTypeError('Specify custom color by using \'Custom (#xxxxxx)\'')
+    elif re.match(r'^Custom \(#[A-Fa-f0-9]{6}\)$', s):
+        return re.findall(r'[A-Fa-f0-9]{6}', s)[0]
+    elif s in get_tunic_color_options():
+        return s
+    else:
+        raise argparse.ArgumentTypeError('Invalid color specified')
+
+def parse_custom_navi_color(s):
+    if s == 'Custom Color':
+        raise argparse.ArgumentTypeError('Specify custom color by using \'Custom (#xxxxxx)\'')
+    elif re.match(r'^Custom \(#[A-Fa-f0-9]{6}\)$', s):
+        return re.findall(r'[A-Fa-f0-9]{6}', s)[0]
+    elif s in get_navi_color_options():
+        return s
+    else:
+        raise argparse.ArgumentTypeError('Invalid color specified')
 
 # a list of the possible settings
 setting_infos = [
@@ -1359,7 +1378,7 @@ setting_infos = [
             'default': 'Kokiri Green',
             'const': 'Kokiri Green',
             'nargs': '?',
-            'choices': get_tunic_color_options(),
+            'type': parse_custom_tunic_color,
             'help': '''\
                     Choose the color for Link's Kokiri Tunic. (default: %(default)s)
                     
@@ -1383,7 +1402,7 @@ setting_infos = [
             'default': 'Goron Red',
             'const': 'Goron Red',
             'nargs': '?',
-            'choices': get_tunic_color_options(),
+            'type': parse_custom_tunic_color,
             'help': '''\
                     Choose the color for Link's Goron Tunic. (default: %(default)s)
                     Color:              Make the Goron Tunic this color.
@@ -1409,7 +1428,7 @@ setting_infos = [
             'default': 'Zora Blue',
             'const': 'Zora Blue',
             'nargs': '?',
-            'choices': get_tunic_color_options(),
+            'type': parse_custom_tunic_color,
             'help': '''\
                     Choose the color for Link's Zora Tunic. (default: %(default)s)
                     Color:              Make the Zora Tunic this color.
@@ -1435,7 +1454,7 @@ setting_infos = [
             'default': 'White',
             'const': 'White',
             'nargs': '?',
-            'choices': get_navi_color_options(),
+            'type': parse_custom_navi_color,
             'help': '''\
                     Choose the color for Navi when she is idle. (default: %(default)s)
                     Color:             Make the Navi this color.
@@ -1461,7 +1480,7 @@ setting_infos = [
             'default': 'Yellow',
             'const': 'Yellow',
             'nargs': '?',
-            'choices': get_navi_color_options(),
+            'type': parse_custom_navi_color,
             'help': '''\
                     Choose the color for Navi when she is targeting an enemy. (default: %(default)s)
                     Color:             Make the Navi this color.
@@ -1487,7 +1506,7 @@ setting_infos = [
             'default': 'Light Blue',
             'const': 'Light Blue',
             'nargs': '?',
-            'choices': get_navi_color_options(),
+            'type': parse_custom_navi_color,
             'help': '''\
                     Choose the color for Navi when she is targeting an NPC. (default: %(default)s)
                     Color:             Make the Navi this color.
@@ -1513,7 +1532,7 @@ setting_infos = [
             'default': 'Green',
             'const': 'Green',
             'nargs': '?',
-            'choices': get_navi_color_options(),
+            'type': parse_custom_navi_color,
             'help': '''\
                     Choose the color for Navi when she is targeting a prop. (default: %(default)s)
                     Color:             Make the Navi this color.
