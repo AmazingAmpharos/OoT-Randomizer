92e8e604a5792da1ddd0f4ec465f67b731ad12c3
Unknown <aaturpie@verizon.net>
2018-10-21 15:31:01 -0600

    decompressor uses same bundle compatibility at compressor

--- Main.py
+++ Main.py
@@ -20,7 +20,7 @@ from Rules import set_rules
 from Fill import distribute_items_restrictive
 from ItemList import generate_itempool
 from Hints import buildGossipHints
-from Utils import default_output_path, is_bundled
+from Utils import default_output_path, is_bundled, subprocess_args
 from version import __version__
 from OcarinaSongs import verify_scarecrow_song_str
 
@@ -181,46 +181,8 @@ def main(settings, window=dummy_window()):
     return worlds[settings.player_num - 1]
 
 
-# From the pyinstaller Wiki: https://github.com/pyinstaller/pyinstaller/wiki/Recipe-subprocess
-# Create a set of arguments which make a ``subprocess.Popen`` (and
-# variants) call work with or without Pyinstaller, ``--noconsole`` or
-# not, on Windows and Linux. Typical use::
-#   subprocess.call(['program_to_run', 'arg_1'], **subprocess_args())
-def subprocess_args(include_stdout=True):
-    # The following is true only on Windows.
-    if hasattr(subprocess, 'STARTUPINFO'):
-        # On Windows, subprocess calls will pop up a command window by default
-        # when run from Pyinstaller with the ``--noconsole`` option. Avoid this
-        # distraction.
-        si = subprocess.STARTUPINFO()
-        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        # Windows doesn't search the path by default. Pass it an environment so
-        # it will.
-        env = os.environ
-    else:
-        si = None
-        env = None
-
-    # ``subprocess.check_output`` doesn't allow specifying ``stdout``::
-    # So, add it only if it's needed.
-    if include_stdout:
-        ret = {'stdout': subprocess.PIPE}
-    else:
-        ret = {}
-
-    # On Windows, running this from the binary produced by Pyinstaller
-    # with the ``--noconsole`` option requires redirecting everything
-    # (stdin, stdout, stderr) to avoid an OSError exception
-    # "[Error 6] the handle is invalid."
-    ret.update({'stdin': subprocess.PIPE,
-                'stderr': subprocess.PIPE,
-                'startupinfo': si,
-                'env': env })
-    return ret
-
-
 def run_process(window, logger, args):
-    process = subprocess.Popen(args, **subprocess_args())
+    process = subprocess.Popen(args, **subprocess_args(True))
     filecount = None
     while True:
         line = process.stdout.readline()
