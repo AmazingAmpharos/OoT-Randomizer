e6960390873a6bb52302bf04227f7e3b949694bd
Nick Langille <nick.langille@gmail.com>
2018-10-30 00:18:37 -0400

    Fix scaling of heart piece models

--- ASM/c/models.c
+++ ASM/c/models.c
@@ -21,23 +21,6 @@ void load_gi_object_file(uint32_t object_file_id, uint8_t *buf) {
     read_file(buf, vrom_start, size);
 }
 
-void models_init() {
-    // Use SOLD OUT model to indicate that an error occured
-    error_gi_obj.gi_id = 0x59;
-    error_gi_obj.buf = heap_alloc(0x4D0);
-    load_gi_object_file(0x148, error_gi_obj.buf);
-
-    for (int i = 0; i < gi_obj_count; i++) {
-        gi_obj_slots[i].gi_id = 0;
-        gi_obj_slots[i].buf = heap_alloc(gi_obj_size);
-    }
-}
-
-void models_reset() {
-    for (int i = 0; i < gi_obj_count; i++) {
-        gi_obj_slots[i].gi_id = 0;
-    }
-}
 
 void load_gi_object(gi_object_t *gi_obj, uint32_t gi_id) {
     gi_obj->gi_id = gi_id;
@@ -62,8 +45,36 @@ gi_object_t *get_gi_object(uint32_t gi_id) {
 void set_gi_object_segment(gi_object_t *gi_obj) {
     z64_disp_buf_t *xlu = &(z64_ctxt.gfx->poly_xlu);
     gSPSegment(xlu->p++, 6, (uint32_t)(gi_obj->buf));
+
     z64_disp_buf_t *opa = &(z64_ctxt.gfx->poly_opa);
     gSPSegment(opa->p++, 6, (uint32_t)(gi_obj->buf));
+}
+
+
+void scale_matrix(float *matrix, float scale_factor) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix[4*i + j] *= scale_factor;
+        }
+    }
+}
+
+void models_init() {
+    // Use SOLD OUT model to indicate that an error occured
+    error_gi_obj.gi_id = 0x59;
+    error_gi_obj.buf = heap_alloc(0x4D0);
+    load_gi_object_file(0x148, error_gi_obj.buf);
+
+    for (int i = 0; i < gi_obj_count; i++) {
+        gi_obj_slots[i].gi_id = 0;
+        gi_obj_slots[i].buf = heap_alloc(gi_obj_size);
+    }
+}
+
+void models_reset() {
+    for (int i = 0; i < gi_obj_count; i++) {
+        gi_obj_slots[i].gi_id = 0;
+    }
 }
 
 typedef void (*pre_draw_gi_obj_fn)(z64_actor_t *actor, z64_ctxt_t *global_ctxt,
@@ -74,6 +85,8 @@ typedef void (*pre_draw_gi_obj_fn)(z64_actor_t *actor, z64_ctxt_t *global_ctxt,
 typedef void (*draw_gi_obj_fn)(z64_ctxt_t *global_ctxt, uint32_t gi_id_minus_1);
 #define draw_gi_obj ((draw_gi_obj_fn)0x800570C0)
 
+#define matrix_stack_pointer ((float **)0x80121204)
+
 void models_draw(z64_actor_t *heart_piece_actor, z64_ctxt_t *global_ctxt) {
     int lookup_gi_id = 0x45;
     gi_object_t *gi_obj = get_gi_object(lookup_gi_id);
@@ -81,5 +94,6 @@ void models_draw(z64_actor_t *heart_piece_actor, z64_ctxt_t *global_ctxt) {
     pre_draw_gi_obj_1(heart_piece_actor, global_ctxt, 0);
     pre_draw_gi_obj_2(heart_piece_actor, global_ctxt, 0);
     set_gi_object_segment(gi_obj);
+    scale_matrix(*matrix_stack_pointer, 16.0);
     draw_gi_obj(global_ctxt, gi_obj->gi_id - 1);
 }
