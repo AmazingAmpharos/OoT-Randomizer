da5dea2a54439654d1bdb4a07a3902f233520acc
krimtonz <krimtonz@gmail.com>
2018-11-03 18:11:46 -0500

    Toggle quick boots on press instead of release, related code cleanup

--- ASM/c/main.c
+++ ASM/c/main.c
@@ -5,18 +5,19 @@
 #include "util.h"
 #include "z64.h"
 
+static uint16_t pad_pressed_raw, 
+	pad, 
+	pad_pressed;
+
 void c_init() {
+	pad_pressed_raw = 0;
+	pad = 0;
+	pad_pressed = 0;
     heap_init();
     gfx_init();
     text_init();
 }
 
-_Bool zu_in_game(void)
-{
-	return (uint32_t)z64_ctxt.state_dtor == z64_state_ovl_tab[3].vram_dtor;
-}
-
-static uint16_t pad_pressed_raw, pad_released, pad, pad_pressed;
 static int button_time[16];
 void c_after_game_state_update() {
     if (z64_game.pause_state == 6 &&
@@ -29,35 +30,23 @@ void c_after_game_state_update() {
 
 	uint16_t z_pad = z64_input_direct.raw.pad;
 	pad_pressed_raw = (pad ^ z_pad) & z_pad;
-	pad_released = (pad ^ z_pad) & ~z_pad;
 	pad = z_pad;
 	pad_pressed = 0;
 	for (int i = 0; i < 16; ++i) {
 		uint16_t p = 1 << i;
-		if (pad & p)
-			++button_time[i];
-		else
-			button_time[i] = 0;
-		if ((pad_pressed_raw & p) || button_time[i] >= 8)
+		if ((pad_pressed_raw & p))
 			pad_pressed |= p;
 	}
 
-	if (zu_in_game() && z64_file.link_age==0) {
+	if (z64_file.link_age==0) {
 
-		
-
-		/*if (z64_ctxt.input[0].raw.dl) {
-			z64_file.equip_boots = 1;
-			z64_UpdateEquipment(&z64_game, &z64_link);
-		}*/
-
-		if (pad_released & 0x0200 && z64_file.iron_boots) {
+		if (pad_pressed & 0x0200 && z64_file.iron_boots) {
 			if (z64_file.equip_boots == 2) z64_file.equip_boots = 1;
 			else z64_file.equip_boots = 2;
 			z64_UpdateEquipment(&z64_game, &z64_link);
 		}
 
-		if ((pad_released & 0x0100) && z64_file.hover_boots) {
+		if ((pad_pressed & 0x0100) && z64_file.hover_boots) {
 			if (z64_file.equip_boots == 3) z64_file.equip_boots = 1;
 			else z64_file.equip_boots = 3;
 			z64_UpdateEquipment(&z64_game, &z64_link);
