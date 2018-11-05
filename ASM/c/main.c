c6766466cc4f34ae65189872f1b8d5545765709c
krimtonz <krimtonz@gmail.com>
2018-11-05 15:06:00 -0600

    Disable quick boots when link is in a state that he wouldn't normally be able to equip them.

--- ASM/c/main.c
+++ ASM/c/main.c
@@ -5,6 +5,13 @@
 #include "util.h"
 #include "z64.h"
 
+#define BLOCK_QUICK_BOOTS (0x00000001 | \
+	0x00000002 | \
+    0x00000080 | \
+    0x00000400 | \
+    0x10000000 | \
+    0x20000000)     
+
 static uint16_t pad_pressed_raw, 
 	pad, 
 	pad_pressed;
@@ -39,17 +46,21 @@ void c_after_game_state_update() {
 	}
 
 	if (z64_file.link_age==0) {
+		// Prevent quick boots when link is in a state that he wouldn't normally be able to pause to switch boots.
 
-		if (pad_pressed & 0x0200 && z64_file.iron_boots) {
-			if (z64_file.equip_boots == 2) z64_file.equip_boots = 1;
-			else z64_file.equip_boots = 2;
-			z64_UpdateEquipment(&z64_game, &z64_link);
-		}
+		if ((z64_link.state_flags_1 & BLOCK_QUICK_BOOTS) == 0) {
 
-		if ((pad_pressed & 0x0100) && z64_file.hover_boots) {
-			if (z64_file.equip_boots == 3) z64_file.equip_boots = 1;
-			else z64_file.equip_boots = 3;
-			z64_UpdateEquipment(&z64_game, &z64_link);
+			if (pad_pressed & 0x0200 && z64_file.iron_boots) {
+				if (z64_file.equip_boots == 2) z64_file.equip_boots = 1;
+				else z64_file.equip_boots = 2;
+				z64_UpdateEquipment(&z64_game, &z64_link);
+			}
+
+			if ((pad_pressed & 0x0100) && z64_file.hover_boots) {
+				if (z64_file.equip_boots == 3) z64_file.equip_boots = 1;
+				else z64_file.equip_boots = 3;
+				z64_UpdateEquipment(&z64_game, &z64_link);
+			}
 		}
 	}
 }
