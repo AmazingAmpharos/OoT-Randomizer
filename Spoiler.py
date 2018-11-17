456f535b6f181075bec9521040a622d0b83d9448
Cuphat <matthew@cuphat.com>
2018-11-16 22:26:27 -0500

    Dynamic line padding in spoiler.
    Now uses the maximum length of the locations or gossip names to determine the line lengths.

--- Spoiler.py
+++ Spoiler.py
@@ -1,6 +1,7 @@
 from version import __version__
 from collections import OrderedDict
 from Item import Item
+from Hints import gossipLocations
 import re
 
 
@@ -12,7 +13,7 @@ class Spoiler(object):
         self.playthrough = {}
         self.locations = {}
         self.metadata = {}
-        self.required_locations = []
+        self.required_locations = {}
         self.hints = {}
 
 
@@ -31,7 +32,7 @@ class Spoiler(object):
             outfile.write('OoT Randomizer Version %s  -  Seed: %s\n\n' % (__version__, self.settings.seed))
             outfile.write('Settings (%s):\n%s' % (self.settings.get_settings_string(), self.settings.get_settings_display()))
 
-            extra_padding = 0 if self.settings.world_count < 2 else 5 if self.settings.world_count < 10 else 6
+            extra_padding = 1 if self.settings.world_count < 2 else 6 if self.settings.world_count < 10 else 7
             if self.settings.world_count > 1:
                 header_world_string = '\n\n{header} [World {world}]:\n\n'
                 header_player_string = '\n\n{header} [Player {player}]:\n\n'
@@ -43,21 +44,22 @@ class Spoiler(object):
                 location_string = '{location}:'
                 item_string = '{item}'
 
+            location_padding = len(max(self.locations[0].keys(), key=len)) + extra_padding
             for world in self.worlds:
                 outfile.write(header_world_string.format(header="Locations", world=world.id+1))
-                outfile.write('\n'.join(['{:{width}} {}'.format(location_string.format(location=location, world=world.id+1), item_string.format(item=item.name, player=item.world.id+1), width=50+extra_padding) for (location, item) in self.locations[world.id].items()]))
+                outfile.write('\n'.join(['{:{width}} {}'.format(location_string.format(location=location, world=world.id+1), item_string.format(item=item.name, player=item.world.id+1), width=location_padding) for (location, item) in self.locations[world.id].items()]))
 
             outfile.write('\n\nPlaythrough:\n\n')
-            outfile.write('\n'.join(['%s: {\n%s\n}' % (sphere_nr, '\n'.join(['{:{width}} {}'.format(('  ' + location_string).format(location=location.name, world=location.world.id+1), item_string.format(item=item.name, player=item.world.id+1), width=50+extra_padding) for (location, item) in sphere.items()])) for (sphere_nr, sphere) in self.playthrough.items()]))
+            outfile.write('\n'.join(['%s: {\n%s\n}' % (sphere_nr, '\n'.join(['  {:{width}} {}'.format(location_string.format(location=location.name, world=location.world.id+1), item_string.format(item=item.name, player=item.world.id+1), width=location_padding) for (location, item) in sphere.items()])) for (sphere_nr, sphere) in self.playthrough.items()]))
 
             if len(self.hints) > 0:
                 for world in self.worlds:
                     outfile.write(header_player_string.format(header="Way of the Hero", player=world.id+1))
-                    outfile.write('\n'.join(['{:{width}} {}'.format(location_string.format(location=location.name, world=location.world.id+1), item_string.format(item=location.item.name, player=location.item.world.id+1), width=50+extra_padding) for location in self.required_locations[world.id]]))
+                    outfile.write('\n'.join(['{:{width}} {}'.format(location_string.format(location=location.name, world=location.world.id+1), item_string.format(item=location.item.name, player=location.item.world.id+1), width=location_padding) for location in self.required_locations[world.id]]))
 
-                from Hints import gossipLocations
+                gossip_padding = len(max([stone.name for stone in gossipLocations.values()], key=len)) + extra_padding
                 for world in self.worlds:
                     hint_ids = sorted(list(world.spoiler.hints.keys()), key=lambda id: gossipLocations[id].name)
                     outfile.write(header_player_string.format(header="Gossip Stone Hints", player=world.id+1))
-                    outfile.write('\n'.join(['{:{width}} {}'.format(location_string.format(location=gossipLocations[id].name if id in gossipLocations else "Unknown", world=world.id+1), re.sub('\x05[\x40\x41\x42\x43\x44\x45\x46\x47]', '', world.spoiler.hints[id].replace('&', ' ').replace('^', ' ')), width=40+extra_padding) for id in hint_ids]))
+                    outfile.write('\n'.join(['{:{width}} {}'.format(location_string.format(location=gossipLocations[id].name if id in gossipLocations else "Unknown", world=world.id+1), re.sub('\x05[\x40\x41\x42\x43\x44\x45\x46\x47]', '', world.spoiler.hints[id].replace('&', ' ').replace('^', ' ')), width=gossip_padding) for id in hint_ids]))
 
