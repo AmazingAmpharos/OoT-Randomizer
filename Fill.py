8217ead4bd36c8a8eb4e3be41c76694c8623b297
Unknown <aaturpie@verizon.net>
2018-11-27 13:29:53 -0700

    Rework logic_no_ocarina_of_time a bit to prevent self locking (#311)

--- Fill.py
+++ Fill.py
@@ -79,9 +79,7 @@ def distribute_items_restrictive(window, worlds, fill_locations=None):
     # of failing compared to other item type. So this way we only have retry
     # the song locations only.
     if not worlds[0].shuffle_song_items:
-        unplaced_songs, unfilled_song_locations = fill_songs(window, worlds, song_locations, songitempool, progitempool)
-        progitempool.extend(unplaced_songs)
-        fill_locations.extend(unfilled_song_locations)
+        fill_songs(window, worlds, song_locations, songitempool, progitempool)
 
     # Put one item in every dungeon, needs to be done before other items are
     # placed to ensure there is a spot available for them
@@ -97,13 +95,8 @@ def distribute_items_restrictive(window, worlds, fill_locations=None):
     # Place all priority items.
     # These items are items that only check if the item is allowed to be
     # placed in the location, not checking reachability. This is important
-    # for things like Ice Traps that can't be found at some locations.
-    # We'll try to place any restricted items here remaining, since the game
-    # is still beatable. These should only happen because some locations
-    # are considered never accessible by the setting.
+    # for things like Ice Traps that can't be found at some locations
     random.shuffle(fill_locations)
-    prioitempool.extend(progitempool)
-    progitempool = []
     fill_restrictive_fast(window, worlds, fill_locations, prioitempool)
 
     # Place the rest of the items.
@@ -149,8 +142,7 @@ def fill_dungeons_restrictive(window, worlds, shuffled_locations, dungeon_items,
     dungeon_items.sort(key=lambda item: sort_order.get(item.type, 1))
 
     # place dungeon items
-    if fill_restrictive(window, worlds, all_state_base_list, shuffled_locations, dungeon_items):
-        raise FillError("Not all dungeon items placed.")
+    fill_restrictive(window, worlds, all_state_base_list, shuffled_locations, dungeon_items)
 
     for world in worlds:
         world.state.clear_cached_unreachable()
@@ -222,8 +214,7 @@ def fill_shops(window, worlds, locations, shoppool, itempool, attempts=15):
             prize_locs = list(locations)
             random.shuffle(prizepool)
             random.shuffle(prize_locs)
-            if fill_restrictive(window, worlds, all_state_base_list, prize_locs, prizepool):
-                raise FillError("Not all shop items placed.")
+            fill_restrictive(window, worlds, all_state_base_list, prize_locs, prizepool)
             logging.getLogger('').info("Shop items placed")
         except FillError as e:
             logging.getLogger('').info("Failed to place shop items. Will retry %s more times", attempts)
@@ -245,13 +236,14 @@ def fill_songs(window, worlds, locations, songpool, itempool, attempts=15):
     unplaced_prizes = [song for song in songpool if song.name not in placed_prizes]
     empty_song_locations = [loc for loc in locations if loc.item is None]
 
+    # Set logic_no_ocarina_of_time to false to allow songs to be placed regardless of that setting.
+    prev_no_ocarina_of_time = worlds[0].logic_no_ocarina_of_time
+
     # List of states with all items
     all_state_base_list = State.get_states_with_items([world.state for world in worlds], itempool)
 
     prizepool_dict = {world.id: [song for song in unplaced_prizes if song.world.id == world.id] for world in worlds}
     prize_locs_dict = {world.id: [loc for loc in empty_song_locations if loc.world.id == world.id] for world in worlds}
-    unplaced_songs = []
-    unfilled_locations = []
 
     # Songs being sent in to this method are tied to their own world.
     # Therefore, let's do this one world at a time. We do this to help
@@ -265,23 +257,20 @@ def fill_songs(window, worlds, locations, songpool, itempool, attempts=15):
                 prize_locs = list(prize_locs_dict[world.id])
                 random.shuffle(prizepool)
                 random.shuffle(prize_locs)
-                fill_restrictive(window, worlds, all_state_base_list, prize_locs, prizepool)
 
-                # extend the progitem pool with the unplaced songs to be placed later
-                unplaced_songs.extend(prizepool)
-                unfilled_locations.extend(prize_locs)
-                logging.getLogger('').info("Songs placed")
+                world.logic_no_ocarina_of_time = False
+                fill_restrictive(window, worlds, all_state_base_list, prize_locs, prizepool, logic_no_ocarina_of_time=prev_no_ocarina_of_time)
+
+                logging.getLogger('').info("Songs placed for world %s", (world.id+1))
             except FillError as e:
-                logging.getLogger('').info("Failed to place songs. Will retry %s more times", attempts)
-                for location in empty_song_locations:
+                logging.getLogger('').info("Failed to place songs for world %s. Will retry %s more times", (world.id+1), world_attempts)
+                for location in prize_locs_dict[world.id]:
                     location.item = None
                 logging.getLogger('').info('\t%s' % str(e))
                 continue
             break
         else:
-            raise FillError('Unable to place songs')
-
-    return (unplaced_songs, unfilled_locations)
+            raise FillError('Unable to place songs in world %d' % (world.id+1))
 
 
 # Places items in the itempool into locations.
@@ -300,7 +289,7 @@ def fill_songs(window, worlds, locations, songpool, itempool, attempts=15):
 # This function will modify the location and itempool arguments. placed items and
 # filled locations will be removed. If this returns and error, then the state of
 # those two lists cannot be guaranteed.
-def fill_restrictive(window, worlds, base_state_list, locations, itempool, count=-1):
+def fill_restrictive(window, worlds, base_state_list, locations, itempool, count=-1, logic_no_ocarina_of_time=False):
     unplaced_items = []
 
     # loop until there are no items or locations
@@ -352,18 +341,19 @@ def fill_restrictive(window, worlds, base_state_list, locations, itempool, count
                 unplaced_items.append(item_to_place)
                 continue
             else:
-                # unplaced items will be placed in later stages as appropriate
-                if not State.can_beat_game(maximum_exploration_state_list):
-                    raise FillError('Game unbeatable: No more spots to place %s [World %d]' % (item_to_place, item_to_place.world.id))
-                else:
-                    unplaced_items.append(item_to_place)
-                    continue
+                # we expect all items to be placed
+                raise FillError('Game unbeatable: No more spots to place %s [World %d]' % (item_to_place, item_to_place.world.id))
 
         # Place the item in the world and continue
         spot_to_fill.world.push_item(spot_to_fill, item_to_place)
         locations.remove(spot_to_fill)
         window.fillcount += 1
         window.update_progress(5 + ((window.fillcount / window.locationcount) * 30))
+
+        if logic_no_ocarina_of_time and spot_to_fill.name == 'Song from Ocarina of Time':
+            spot_to_fill.world.logic_no_ocarina_of_time = True
+            if not State.can_beat_game(maximum_exploration_state_list):
+                raise FillError('Game unbeatable: Tried to place %s [World %d] (required) in a removed location' % (item_to_place, item_to_place.world.id))
 
         # decrement count
         count -= 1
@@ -373,9 +363,6 @@ def fill_restrictive(window, worlds, base_state_list, locations, itempool, count
         raise FillError('Could not place the specified number of item. %d remaining to be placed.' % count)
     # re-add unplaced items that were skipped
     itempool.extend(unplaced_items)
-
-    # return whether some items were unplaced
-    return len(unplaced_items) > 0
 
 
 # This places items in the itempool into the locations
--- version.py
+++ version.py
@@ -1,1 +1,1 @@
-__version__ = '3.5.7 f.LUM'
+__version__ = '3.5.8 f.LUM'
