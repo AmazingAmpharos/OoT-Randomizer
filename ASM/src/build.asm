4c64a3546ef46c6c15db077e7e82f262ed7b5f64
krimtonz <krimtonz@gmail.com>
2018-12-10 16:12:30 -0600

    Jabu elevator

--- ASM/src/build.asm
+++ ASM/src/build.asm
@@ -56,6 +56,7 @@ DebugOutput:
 .include "rainbow_bridge.asm"
 .include "gossip_hints.asm"
 .include "potion_shop.asm"
+.include "jabu_elevator.asm"
 .endarea
 
 .headersize (0x80405000 - 0x034B3000)
