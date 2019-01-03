3df9107549d5b398c49f24aa65b54b5141b97ce4
krimtonz <krimtonz@gmail.com>
2019-01-03 16:19:34 -0600

    Ocarina DPAD WIP

--- ASM/c/dpad.h
+++ ASM/c/dpad.h
@@ -3,27 +3,26 @@
 
 #include "z64.h"
 
-#define BLOCK_QUICK_BOOTS (0x00000001 | \
+#define BLOCK_DPAD (0x00000001 | \
 	0x00000002 | \
     0x00000080 | \
     0x00000400 | \
     0x10000000 | \
     0x20000000)
 
-#define DISPLAY_QUICKBOOTS ((z64_file.link_age==0) && \
-                           (z64_file.iron_boots || z64_file.hover_boots))
+#define DISPLAY_DPAD       (z64_file.iron_boots || z64_file.hover_boots || z64_file.items[0x07] == 0x07 || z64_file.items[0x08] == 0x08)
 
-#define CAN_USE_QUICKBOOTS (((z64_link.state_flags_1 & BLOCK_QUICK_BOOTS) == 0) && \
+#define CAN_USE_DPAD       (((z64_link.state_flags_1 & BLOCK_DPAD) == 0) && \
                            ((uint32_t)z64_ctxt.state_dtor==z64_state_ovl_tab[3].vram_dtor) && \
                            (z64_file.file_index!=0xFF) && \
                            ((z64_event_state_1 & 0x20)==0) && \
-                            DISPLAY_QUICKBOOTS)
+                           DISPLAY_DPAD)
 #define DPAD_L 0x0200
 #define DPAD_R 0x0100
 #define DPAD_D 0x0400
 
-void handle_quickboots();
-void draw_quickboots();
-void quickboots_init();
+void handle_dpad();
+void draw_dpad();
+void dpad_init();
 
 #endif
--- ASM/src/dpad.asm
+++ ASM/src/dpad.asm
@@ -1,7 +1,7 @@
-qb_draw:
+dpad_draw:
     addiu   sp, sp, -0x10
     sw      ra, 0(sp)
-    jal     draw_quickboots
+    jal     draw_dpad
     nop
     lw      t6, 0x1C44(s6)
     lui     t8, 0xDB06
