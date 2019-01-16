650915b14ca64c734a5136a7f273b71bcde84a58
Unknown <aaturpie@verizon.net>
2019-01-16 07:25:37 -0700

    Fix Indentation

--- Patches.py
+++ Patches.py
@@ -40,7 +40,7 @@ def patch_rom(spoiler:Spoiler, world:World, rom:LocalRom):
     rom.write_byte(0x253C0E2, 0x03) # Moves sheik from pedestal
 
     # Fix Ice Cavern Alcove Camera
-   if not world.dungeon_mq['Ice Cavern']:
+    if not world.dungeon_mq['Ice Cavern']:
         rom.write_byte(0x2BECA25,0x01);
         rom.write_byte(0x2BECA2D,0x01);
 
