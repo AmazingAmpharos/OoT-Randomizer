69078e06b58d236bebd8f74e6820e5d9f066ad80
mzxrules <mzxrules@gmail.com>
2019-01-16 23:57:48 -0500

    Better rainbow sword animation

--- ASM/c/rainbow_sword.h
+++ ASM/c/rainbow_sword.h
@@ -1,1 +1,37 @@
+#include <stdint.h>
+
+typedef struct
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} colorRGB_t;
+
+typedef struct
+{
+    union
+    {
+        struct
+        {
+            uint8_t r;
+            uint8_t g;
+            uint8_t b;
+        };
+        colorRGB_t color;
+    };
+    uint8_t a;
+} colorRGBA_t;
+
+
+static colorRGB_t colors[] =
+{
+    { 0xE0, 0x10, 0x10 }, //red
+    { 0xE0, 0xE0, 0x10 }, //yellow
+    { 0x10, 0xE0, 0x10 }, //green
+    { 0x10, 0xE0, 0xE0 }, //cyan
+    { 0x10, 0x10, 0xE0 }, //blue
+    { 0xE0, 0x10, 0xE0 }, //purple
+    { 0xE0, 0x10, 0x10 }, //red
+};
+
 void update_color();
