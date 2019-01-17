6619d81e186d38f55e96ff9ea66d0b4bc0fb52d2
Unknown <aaturpie@verizon.net>
2019-01-17 08:07:20 -0700

    Move unnecessary definitions out of the header file

--- ASM/c/rainbow_sword.c
+++ ASM/c/rainbow_sword.c
@@ -10,6 +10,40 @@ const static uint32_t CYCLE_FRAMES_2 = 12;
 
 extern uint8_t RAINBOW_SWORD_ENABLED;
 
+typedef struct
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} colorRGB_t;
+
+typedef struct
+{
+    union
+    {
+        struct
+        {
+            uint8_t r;
+            uint8_t g;
+            uint8_t b;
+        };
+        colorRGB_t color;
+    };
+    uint8_t a;
+} colorRGBA_t;
+
+
+static colorRGB_t colors[] =
+{
+    { 0xE0, 0x10, 0x10 }, //red
+    { 0xE0, 0xE0, 0x10 }, //yellow
+    { 0x10, 0xE0, 0x10 }, //green
+    { 0x10, 0xE0, 0xE0 }, //cyan
+    { 0x10, 0x10, 0xE0 }, //blue
+    { 0xE0, 0x10, 0xE0 }, //purple
+    { 0xE0, 0x10, 0x10 }, //red
+};
+
 static colorRGB_t get_color(uint32_t f, uint32_t step_frames)
 {
     int index;
