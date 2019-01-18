294b44bac29409c971f0623846ebbeaf91f85747
kevinPal <kevinpalani@gmail.com>
2019-01-18 01:02:45 -0600

    scene flags

--- ASM/src/cow.asm
+++ ASM/src/cow.asm
@@ -1,8 +1,15 @@
 cow_item_hook:
     lw      a0, 0x0020 (sp)
     nop
-    lh      a2, 0x014 (a0) ; Load cow id
-    addiu   a2, a2, 0x0014 ; give item id = cow id + milk:q
+    lh      a2, 0x014 (a0) ; Load cow id (1 or 2)
+    lb      t0, 0x1D44 (a1) ; Load scene collect flag
+    and     at, a2, t0
+    bnezl    at, @@return
+    addiu   a2, r0, 0x50 ; Give milk if scene flag is set
+    or      t0, t0, a2
+    sb      t0, 0x1D44 (a1)
+    addiu   a2, a2, 0x0014 ; give item id = cow id + milk
+@@return:
     jr      ra
     nop
 
