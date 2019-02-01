d565a9ceaceea379a68907b18d43fac4dc086e41
rattus128 <rattus128@gmail.com>
2019-02-02 10:38:14 +1100

    Make JSON dev easier by printing some surrounding text on error
    Its a little jank to read these in the GUI popup but they come up real nice
    in the CLI.

--- World.py
+++ World.py
@@ -13,6 +13,7 @@ import copy
 import io
 import json
 import random
+import re
 
 class World(object):
 
@@ -103,7 +104,13 @@ class World(object):
         with io.open(file_path, 'r') as file:
             for line in file.readlines():
                 json_string += line.split('#')[0].replace('\n', ' ')
-        region_json = json.loads(json_string)
+        json_string = re.sub(' +', ' ', json_string)
+        try:
+            region_json = json.loads(json_string)
+        except json.JSONDecodeError as error:
+            raise Exception("JSON parse error around text:\n" + \
+                            json_string[error.pos-35:error.pos+35] + "\n" + \
+                            "                                   ^^\n")
 
         for region in region_json:
             new_region = Region(region['region_name'])
