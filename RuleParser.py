09d52a553fb37cd3e3e560c04b01afaa8ca18dfb
Zannick <bswolf@google.com>
2019-06-16 13:40:25 -0700

    Constant folding in the Rule Parser.

--- RuleParser.py
+++ RuleParser.py
@@ -15,6 +15,9 @@ for item in item_table:
 
 event_name = re.compile(r'\w+')
 
+def isliteral(expr):
+    return isinstance(expr, (ast.Num, ast.Str, ast.Bytes, ast.NameConstant))
+
 
 class Rule_AST_Transformer(ast.NodeTransformer):
 
@@ -37,13 +40,8 @@ class Rule_AST_Transformer(ast.NodeTransformer):
                 args=[ast.Str(escaped_items[node.id])],
                 keywords=[])
         elif node.id in self.world.__dict__:
-            return ast.Attribute(
-                value=ast.Attribute(
-                    value=ast.Name(id='state', ctx=ast.Load()),
-                    attr='world',
-                    ctx=ast.Load()),
-                attr=node.id,
-                ctx=ast.Load())
+            # Settings are constant
+            return ast.parse('%r' % self.world.__dict__[node.id], mode='eval').body
         elif node.id in State.__dict__:
             return ast.Call(
                 func=ast.Attribute(
@@ -95,13 +93,8 @@ class Rule_AST_Transformer(ast.NodeTransformer):
             raise Exception('Parse Error: second value must be a number. Got %s' % item.__class__.__name__, self.current_spot.name, ast.parse(node, False))
 
         if isinstance(count, ast.Name):
-            count = ast.Attribute(
-                value=ast.Attribute(
-                    value=ast.Name(id='state', ctx=ast.Load()),
-                    attr='world',
-                    ctx=ast.Load()),
-                attr=count.id,
-                ctx=ast.Load())
+            # Must be a settings constant
+            count = ast.parse('%r' % self.world.__dict__[count.id], mode='eval').body
 
         if iname in escaped_items:
             iname = escaped_items[iname]
@@ -171,7 +164,7 @@ class Rule_AST_Transformer(ast.NodeTransformer):
     def visit_Compare(self, node):
         def escape_or_string(n):
             if isinstance(n, ast.Name) and n.id in escaped_items:
-                return ast.Str(escaped_items[n])
+                return ast.Str(escaped_items[n.id])
             elif not isinstance(n, ast.Str):
                 return self.visit(n)
             return n
@@ -180,6 +173,80 @@ class Rule_AST_Transformer(ast.NodeTransformer):
         node.comparators = list(map(escape_or_string, node.comparators))
         node.ops = list(map(self.visit, node.ops))
 
+        # if all the children are literals now, we can evaluate
+        if isliteral(node.left) and all(map(isliteral, node.comparators)):
+            # either we turn the ops into operator functions to apply (lots of work),
+            # or we compile, eval, and reparse the result
+            res = eval(compile(ast.Expression(node), '<string>', 'eval'))
+            return ast.parse('%r' % res, mode='eval').body
+        return node
+
+
+    def visit_UnaryOp(self, node):
+        # visit the children first
+        self.generic_visit(node)
+        # if all the children are literals now, we can evaluate
+        if isliteral(node.operand):
+            res = eval(compile(ast.Expression(node), '<string>', 'eval'))
+            return ast.parse('%r' % res, mode='eval').body
+        return node
+
+
+    def visit_BinOp(self, node):
+        # visit the children first
+        self.generic_visit(node)
+        # if all the children are literals now, we can evaluate
+        if isliteral(node.left) and isliteral(node.right):
+            res = eval(compile(ast.Expression(node), '<string>', 'eval'))
+            return ast.parse('%r' % res, mode='eval').body
+        return node
+
+
+    def visit_BoolOp(self, node):
+        # TO BE FIXED: Names that correspond to items, and Strs should be put
+        # in a has_any_of/has_all_of list
+        # Everything else must be visited, then can be removed/reduced to.
+        # visit the children first
+        self.generic_visit(node)
+
+        early_return = isinstance(node.op, ast.Or)
+        items = []
+        new_values = []
+        # if any elt is True(And)/False(Or), we can omit it
+        # if any is False(And)/True(Or), the whole node can be replaced with it
+        for elt in list(node.values):
+            if isinstance(elt, ast.Str):
+                items.append(elt.s)
+            elif isinstance(elt, ast.Name) and elt.id in escaped_items:
+                items.append(escaped_items[elt.id])
+            else:
+                # It's possible this returns a single item check,
+                # but it's already wrapped in a Call.
+                elt = self.visit(elt)
+                if isinstance(elt, ast.NameConstant):
+                    if elt.value == early_return:
+                        return elt
+                    # else omit it
+                else:
+                    new_values.append(elt)
+
+        # package up the remaining items and values
+        if not items and not new_values:
+            # all values were True(And)/False(Or)
+            return ast.NameConstant(not early_return)
+
+        if items:
+            node.values = [ast.Call(
+                func=ast.Attribute(
+                    value=ast.Name(id='state', ctx=ast.Load()),
+                    attr='has_any_of' if early_return else 'has_all_of',
+                    ctx=ast.Load()),
+                args=[ast.Tuple(tuple(items))],
+                keywords=[])] + new_values
+        else:
+            node.values = new_values
+        if len(node.values) == 1:
+            return node.values[0]
         return node
 
 
