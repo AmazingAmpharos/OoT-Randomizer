fc89200cbe0ae0dcfd1a716e6f317ba538b38704
rattus128 <rattus128@gmail.com>
2019-07-14 10:56:38 +1000

    Rework great fairy health refills
    Restore the vanilla behaviour of a full health and magic refill on the
    first visit to a great fairy. Both the first visit and the revist
    refill are done without any cutscene. The greate fairy will now no
    longer appear in any case.

--- ASM/c/ganon.c
+++ ASM/c/ganon.c
@@ -1,4 +1,5 @@
 #include "ganon.h"
+#include "refill.h"
 
 #include "z64.h"
 
@@ -6,8 +7,7 @@ uint8_t NO_ESCAPE_SEQUENCE = 0;
 
 void check_ganon_entry() {
     if (NO_ESCAPE_SEQUENCE && z64_file.entrance_index == 0x0517) {
-        z64_file.refill_hearts = 0x140;
-        z64_file.magic = z64_file.magic_capacity_set * 0x30;
+        health_and_magic_refill();
     }
 }
 
