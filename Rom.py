93fdbf31a09f93642faefeb8f1e28e7b990c8667
Unknown <aaturpie@verizon.net>
2019-08-05 02:51:47 -0600

    Blank rom will search the install directory for the ZOOTDEC (Issue #819)

--- Rom.py
+++ Rom.py
@@ -26,7 +26,7 @@ class Rom(BigStream):
         if file is None:
             return
 
-        decomp_file = 'ZOOTDEC.z64'
+        decomp_file = local_path('ZOOTDEC.z64')
 
         os.chdir(local_path())
 
