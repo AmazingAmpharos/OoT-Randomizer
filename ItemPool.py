f74daec40fd490e8cc804baa042edd4c7754d370
r0bd0g <42326313+r0bd0g@users.noreply.github.com>
2019-08-10 18:04:12 -0300

    Add Drop Locations

--- ItemPool.py
+++ ItemPool.py
@@ -477,6 +477,7 @@ droplocations = {
     'Wandering Bugs': 'Bugs',
     'Fairy Pot': 'Fairy',
     'Free Fairies': 'Fairy',
+    'Wall Switch Fairy': 'Fairy',
     'Butterfly Fairy': 'Fairy',
     'Gossip Stone Fairy': 'Fairy',
     'Bean Plant Fairy': 'Fairy',
