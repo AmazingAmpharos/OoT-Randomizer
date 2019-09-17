17f375d6e76e3008affe66ae9bb79e738f61a01e
Roman971 <romanlasnier@hotmail.com>
2019-09-17 22:30:54 +0800

    Minor asm cleanup

--- ASM/src/king_zora.asm
+++ ASM/src/king_zora.asm
@@ -7,10 +7,10 @@ MOVED_ADULT_KING_ZORA:
 kz_moved_check:
     la      t0, SAVE_CONTEXT
     lhu     t1, 0x0EDA(t0)
-    andi    t1, t1, 0x08                ; "Moved King Zora" Flag
+    andi    t1, t1, 0x0008              ; "Moved King Zora" Flag
     bnez    t1, @@return_true           ; if the flag is set, return true
     lw      t1, 0x0004(t0)              ; else, check link's age
-    bnez	t1, @@return_false          ; if child, return false
+    bnez    t1, @@return_false          ; if child, return false
     lb      t1, MOVED_ADULT_KING_ZORA   ; else, check if adult kz should be moved
     bnez    t1, @@return_true           ; if it should, return true
     nop                                 ; else, return false
