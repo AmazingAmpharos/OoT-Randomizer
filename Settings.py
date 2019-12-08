4f0ef3737fe76097f0f14046c7da519508473861
Zannick <bswolf@google.com>
2019-12-08 14:39:21 -0800

    Omit disabled settings from the spoiler.

--- Settings.py
+++ Settings.py
@@ -222,6 +222,7 @@ class Settings:
                 new_value = self.get_dependency(info.name)
                 if new_value != None:
                     self.__dict__[info.name] = new_value
+                    self._disabled.add(info.name)
 
         self.settings_string = self.get_settings_string()
         self.numeric_seed = self.get_numeric_seed()
@@ -257,13 +258,15 @@ class Settings:
         if self.world_count > 255:
             self.world_count = 255
 
+        self._disabled = set()
         self.settings_string = self.get_settings_string()
         self.distribution = Distribution(self)
         self.update_seed(self.seed)
 
 
     def to_json(self):
-        return {setting.name: self.__dict__[setting.name] for setting in setting_infos if setting.shared}
+        return {setting.name: self.__dict__[setting.name] for setting in setting_infos
+                if setting.shared and setting.name not in self._disabled}
 
 
 # gets the randomizer settings, whether to open the gui, and the logger level from command line arguments
