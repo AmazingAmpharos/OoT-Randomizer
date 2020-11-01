e398892663fc3c97892deae837d7f8cd426a8ea6
Louis <35883445+louist103@users.noreply.github.com>
2020-11-01 18:41:04 -0500

    Call TitleInit_Setup

--- ASM/c/reset.c
+++ ASM/c/reset.c
@@ -1,10 +1,9 @@
 #include "reset.h"
-typedef void(*title_init)();
-#define title_init  ((title_init) 0x808007B0)
-typedef void(*file_init)();	
-#define z64_file_init ((file_init) 0x80051aa0)
 
-static unsigned int s_reset_delay = 10;
+typedef void(*title_init_setup)(z64_ctxt_t* ctxt);
+#define title_init_setup  ((title_init_setup) 0x800A0748)
+
+static unsigned short s_reset_delay = 10;
 
 void wait_for_reset_combo(){
 	z64_controller_t pad_pressed = z64_game.common.input[0].raw;
@@ -13,11 +12,7 @@ void wait_for_reset_combo(){
 			s_reset_delay--;
 		}
 		else {
-			
-			z64_file_init();
-			z64_game.common.state_continue = 0;
-			z64_game.common.next_ctor = (void*)title_init;
-			z64_game.common.next_size = sizeof(char[0x1E8]);
+			title_init_setup(&z64_game.common);
 		}
 	}
 	else {
