7efe03848fe41e47375f91fb0593eb367a3eafad
louist103 <35883445+louist103@users.noreply.github.com>
2020-11-02 13:39:34 -0500

    Fix audio issue

--- ASM/c/reset.c
+++ ASM/c/reset.c
@@ -2,6 +2,9 @@
 
 typedef void(*title_init_setup)(z64_ctxt_t* ctxt);
 #define title_init_setup  ((title_init_setup) 0x800A0748)
+typedef void (*set_bgm)(unsigned int ID);
+#define SET_BGM ((set_bgm) 0x800CAA70)
+#define BGM_STOP 0x100000FF
 
 static unsigned short s_reset_delay = 10;
 
@@ -12,6 +15,7 @@ void wait_for_reset_combo(){
 			s_reset_delay--;
 		}
 		else {
+			SET_BGM(BGM_STOP);
 			title_init_setup(&z64_game.common);
 		}
 	}
