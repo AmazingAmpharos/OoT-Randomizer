66573921faada52f509cdd3c55c6decb386d8783
DobbyDigital <lamontagne.steven@gmail.com>
2021-02-17 16:06:05 -0500

    Add preset and hint distribution
    - Updated the DDR preset in presets_default.json to use current settings
    - Added ddr.json in data/Hints for hint distribution currently used by DDR community. Utilises vague named-item hints.
    - Modified  defaultHintDists in Hints.py to include DDR hint distro.

--- Hints.py
+++ Hints.py
@@ -25,7 +25,7 @@ bingoBottlesForHints = (
 )
 
 defaultHintDists = [
-    'balanced.json', 'bingo.json', 'scrubs.json', 'strong.json', 'tournament.json', 'useless.json', 'very_strong.json'
+    'balanced.json', 'bingo.json', 'ddr.json', 'scrubs.json', 'strong.json', 'tournament.json', 'useless.json', 'very_strong.json'
 ]
 
 class RegionRestriction(Enum):
